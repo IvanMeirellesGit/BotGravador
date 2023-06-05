69bdae835f6eeafa52264cf50f514460c3827b6b
Ivan Meirelles <hackermeirelles+github@gmail.com>
2023-06-05 09:38:00 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,12 +2,17 @@ import pyautogui
 import time
 
 p = pyautogui
-
-
 def abrirNav():
     p.press('win')
-    p.write("Edge")
+    time.sleep(0.5)
+    p.write("twitch.tv/cellbit")
+    time.sleep(0.5)
     p.press("Enter")
+    time.sleep(12)
+    p.press("f")
 
 
-abrirNav()
+def abrirOBS():
+    p.press('win')
+    time.sleep(0.5)
+    p.write('obs')
